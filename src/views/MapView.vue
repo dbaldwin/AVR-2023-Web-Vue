622c8d795afafde0527d872d6444e7cd5cbf9d5b
Dennis Baldwin <dennisbaldwin@gmail.com>
2023-03-07 16:53:13 -0600

    Map proof of concept

---
+++ src/views/MapView.vue
@@ -0,0 +1,25 @@
+<template>
+  <GoogleMap
+    api-key="AIzaSyA5dRMkbJ6t_cQqrCIuekJd4nJyVzEeSdY"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="markerOptions" />
+  </GoogleMap>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 }
+    const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
+
+    return { center, markerOptions }
+  }
+})
+</script>
