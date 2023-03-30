ef33bc8ffec95b0b892adfccffa2c63e9ddab614
Dennis Baldwin <dennisbaldwin@gmail.com>
2023-03-30 10:49:53 -0500

    Basic test cases for relative positioning

--- src/views/ScanView.vue
+++ src/views/ScanView.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <button @click="captureHome">Capture Home Location</button>&nbsp;
+        <button @click="takeOff">Takeoff</button>&nbsp;
         <button @click="flyScanPattern">Fly Scan Pattern</button>&nbsp;
+        <button @click="land">Land</button>&nbsp;
         <button @click="goHome">Go Home</button>
     </div>
 </template>
@@ -37,16 +39,36 @@ export default defineComponent({
             client.publish('avr/fcm/capture_home', JSON.stringify({}))
         }
 
+        const takeOff = () => {
+            client.publish('avr/fcm/actions', JSON.stringify({ "action": "takeoff", "payload": { "alt": 5 } }))
+        }
+
+        const land = () => {
+            console.log('land')
+            client.publish('avr/fcm/actions', JSON.stringify({ "action": "land", "payload": {} }))
+        }
+
+
         const flyScanPattern = async () => {
 
-            const leg1 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 1, "e": 0, "d": -3, "rel": 1, "heading": 0 } })
+            const forward = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 0.1, "e": 0, "d": 0, "rel": true, "heading": 0 } })
+            const right = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 0, "e": 0.1, "d": 0, "rel": true, "heading": 0 } })
+            const left = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 0, "e": -0.1, "d": 0, "rel": true, "heading": 0 } })
+
+            const commands = [forward, right, forward, left, forward, right, forward, left, forward, right, forward, left, forward];
+
+            for await (const command of commands) {
+                client.publish("avr/fcm/actions", command);
+                console.log(command);
+                await wait(5000);
+            }
+
             // const leg2 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 5, "e": 5, "d": -3, "rel": 1, "heading": 0 } })
             // const leg3 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 10, "e": 5, "d": -3, "rel": 1, "heading": 0 } })
             // const leg4 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 10, "e": 0, "d": -3, "rel": 1, "heading": 0 } })
             // const leg5 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 15, "e": 0, "d": -3, "rel": 1, "heading": 0 } })
             // const leg6 = JSON.stringify({ "action": "goto_location_ned", "payload": { "n": 15, "e": 5, "d": -3, "rel": 1, "heading": 0 } })
 
-            client.publish("avr/fcm/actions", leg1)
 
             // await wait(5000)
             // client.publish("avr/fcm/actions", leg2)
@@ -72,7 +94,9 @@ export default defineComponent({
 
         return {
             captureHome,
+            takeOff,
             flyScanPattern,
+            land,
             goHome
         }
     }
